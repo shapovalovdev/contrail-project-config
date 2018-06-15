376526c4fb1751ac8f3f55e31395630d9eedbcc4
Michal Krawczyk <michal.krawczyk@codilime.com>
2018-06-15 02:49:25 -0800

    Revert "Make sure the correct revision is checked out by Zuul for contrail-vnc-base based jobs"
    This reverts commit 4fe07b2f3bc9c3525f5feff28d4b103161fa0cf6.
    
    Change-Id: I34e290461fcb6643412f81e5efaef9f8894de10a

--- roles/repo-sandbox-prepare/files/manifest_translator.py
+++ roles/repo-sandbox-prepare/files/manifest_translator.py
@@ -18,46 +18,6 @@ def get_head_branch(path):
     return head[:-1]
 
 
-def get_remote_name(path):
-    remotes = subprocess.check_output(['git', 'remote'], cwd=path).strip().split('\n')
-    if len(remotes) != 1:
-        msg = "Repository at {} has multiple or no remotes configured!".format(path)
-        raise RuntimeError(msg)
-    return remotes[0]
-
-
-def get_project_name(path, remote_name):
-    """Retrieve the project name from git config"""
-    url = subprocess.check_output(
-        ['git', 'config', '--get', 'remote.{}.url'.format(remote_name)],
-        cwd=path)
-    project_name = url.split('/')[-1].split('.')[0]
-    return project_name
-
-
-def verify_head_sha(path, manifest_revision):
-    """Verify whether the revision specified in manifest.xml file matches
-    the one checked out by Zuul"""
-    head_sha = get_head_commit_sha(path)
-    remote_name = get_remote_name(path)
-    project_name = get_project_name(path, remote_name)
-    manifest_sha = subprocess.check_output(
-        ['git', 'rev-parse', 'remotes/{}/{}'.format(remote_name, manifest_revision)],
-        cwd=path).strip()
-    if not manifest_sha == head_sha:
-        msg = "Revision mismatch for project: {}!\n".format(project_name)
-        msg += "Manifest revision: \t{}\n".format(manifest_sha)
-        msg += "Zuul revision: \t{}\n".format(head_sha)
-        msg += "The revision checked-out by Zuul should be the same as the\
-                revision coming from the Android Repo's manifest.xml file.\
-                To change the Zuul revision, you can use the 'override-checkout'\
-                config option under specific entry in the 'required-projects'\
-                section of the project configuration. See\
-                https://docs.openstack.org/infra/zuul/user/config.html#attr-job.override-checkout\
-                for details."
-        raise RuntimeError(msg)
-
-
 def dump_xml(node):
     return etree.tostring(node, pretty_print=True).decode()
 
@@ -144,14 +104,10 @@ def snapshot(args):
         zuul_var = yaml.load(zuul_var_file)
     for project in manifest.xpath('//project'):
         name = project.attrib['name']
-        manifest_revision = project.attrib['revision'] if 'revision' in project.attrib else 'master'
-
         zuul_project = get_project(zuul_var, name)
-        project_path = '{}/{}'.format(
-            zuul_var['executor']['work_root'],
-            zuul_project['src_dir'])
-        verify_head_sha(project_path, manifest_revision)
-        sha = get_head_commit_sha(project_path)
+        sha = get_head_commit_sha(
+            zuul_var['executor']['work_root'] +
+            '/' + zuul_project['src_dir'])
         project.attrib['revision'] = sha
     return manifest
 
