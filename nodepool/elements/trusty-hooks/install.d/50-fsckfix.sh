6b61e5a69c45b4e973ebd5d9f35b0e257b637543
Alexey Osorgin <alexey.osorgin@gmail.com>
2019-05-14 12:35:34 +0300

    Trace cat /etc/default/rcS

--- nodepool/elements/trusty-hooks/install.d/50-fsckfix.sh
+++ nodepool/elements/trusty-hooks/install.d/50-fsckfix.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Automatically repair filesystems with inconsistencies during boot
-echo FSCKFIX=yes > /etc/default/rcS
+if [ 'trusty' == $DIB_RELEASE ]; then
+    echo Automatically repair filesystems with inconsistencies during boot
+    echo FSCKFIX=yes > /etc/default/rcS
+
+    echo Trace /etc/default/rcS
+    cat /etc/default/rcS
+fi
